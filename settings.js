2f9c9802ebf122b23a22d59d06c03dafd1b82582
JanuMAG <96702653+JanuMAG@users.noreply.github.com>
2022-07-22 15:33:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['94704515410']
+global.premium = ['94704515410']
+global.ownernomer = '94704515410'
 global.ownername = 'Dark Alpha'
 global.botname = 'BIXBY-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
